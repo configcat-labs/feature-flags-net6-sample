937d74a4d710dc3b964a2f41aea7924278a59062
Rox H <roxana.halati@gmail.com>
2022-10-06 19:31:22 +0300

    added sample app

---
+++ exampleApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ConfigCat.Client;
+
+
+namespace exampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var client = new ConfigCatClient("YOUR-SDK-KEY");
+
+            var mailingListEnabled = client.GetValue("mailingListEnabled", false);
+
+            Console.WriteLine("mailingListEnabled's value from ConfigCat: " + mailingListEnabled);
+
+
+            if (mailingListEnabled == true)
+            {
+ 
+                ViewBag.display = "display:inline-block";
+            }
+            else if (mailingListEnabled == false)
+            {
+                ViewBag.display = "display:none";
+
+            }
+          
+            return View("Index");
+        }
+    }
+}
